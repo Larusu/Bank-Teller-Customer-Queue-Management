e9d959f2be4572349fb5d370a57200034a97353f
JhervisArevalo <jrareval110104@gmail.com>
2025-07-24 22:52:07 +0800

    dsign

--- ProjectFolder/main.cpp
+++ ProjectFolder/main.cpp
@@ -1,7 +1,19 @@
 #include "AppController.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main() 
 {
-    AppController app;
-    app.run();
+    // AppController app;
+    // app.run();
+
+    cout << "╔══════════════╦═════════════╦═══════════════╗" << "\n";
+    cout << "╠	              BANK TELLER                 ╣	" << "\n";
+    cout << "├──────────────╩─────────────╩───────────────┤	" << "\n";
+    cout << "|  1  |     Serve Next Customer              |" << "\n";
+
+
+
 }
